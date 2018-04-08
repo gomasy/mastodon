8c571a8a8a51f7e4ddcf2dcc32b7b1f8f362c78c
Genbu Hase <hasegenbu@gmail.com>
2018-04-08 16:56:25 +0900

    [New] Implement a feature of quote

--- app/javascript/mastodon/features/compose/components/quote_indicator.js
+++ app/javascript/mastodon/features/compose/components/quote_indicator.js
@@ -12,8 +12,8 @@ const messages = defineMessages({
   cancel: { id: 'quote_indicator.cancel', defaultMessage: 'Cancel' },
 });
 
-export default @injectIntl
-class QuoteIndicator extends ImmutablePureComponent {
+@injectIntl
+export default class QuoteIndicator extends ImmutablePureComponent {
 
   static contextTypes = {
     router: PropTypes.object,
@@ -30,7 +30,7 @@ class QuoteIndicator extends ImmutablePureComponent {
   }
 
   handleAccountClick = (e) => {
-    if (e.button === 0 && !(e.ctrlKey || e.metaKey)) {
+    if (e.button === 0) {
       e.preventDefault();
       this.context.router.history.push(`/accounts/${this.props.status.getIn(['account', 'id'])}`);
     }
