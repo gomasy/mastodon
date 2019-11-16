8f4b142a710b8c2c372be5d530220841a9391043
Gomasy <nyan@gomasy.jp>
2019-11-16 11:16:07 +0900

    Revert "Fix missing importFetchedStatuses"
    This reverts commit 175f5a0bd107926595d38b9fd890b679612dbcc4.

--- app/javascript/mastodon/actions/bookmarks.js
+++ app/javascript/mastodon/actions/bookmarks.js
@@ -1,5 +1,4 @@
 import api, { getLinks } from '../api';
-import { importFetchedStatuses } from './importer';
 
 export const BOOKMARKED_STATUSES_FETCH_REQUEST = 'BOOKMARKED_STATUSES_FETCH_REQUEST';
 export const BOOKMARKED_STATUSES_FETCH_SUCCESS = 'BOOKMARKED_STATUSES_FETCH_SUCCESS';
@@ -19,7 +18,6 @@ export function fetchBookmarkedStatuses() {
 
     api(getState).get('/api/v1/bookmarks').then(response => {
       const next = getLinks(response).refs.find(link => link.rel === 'next');
-      dispatch(importFetchedStatuses(response.data));
       dispatch(fetchBookmarkedStatusesSuccess(response.data, next ? next.uri : null));
     }).catch(error => {
       dispatch(fetchBookmarkedStatusesFail(error));
@@ -60,7 +58,6 @@ export function expandBookmarkedStatuses() {
 
     api(getState).get(url).then(response => {
       const next = getLinks(response).refs.find(link => link.rel === 'next');
-      dispatch(importFetchedStatuses(response.data));
       dispatch(expandBookmarkedStatusesSuccess(response.data, next ? next.uri : null));
     }).catch(error => {
       dispatch(expandBookmarkedStatusesFail(error));
